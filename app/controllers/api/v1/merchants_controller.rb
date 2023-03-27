ad6f1de0b36c803f68b4a7064c6a2edb8fd5d968
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-03-27 14:02:01 -0700

    Fix: update index and show actions to include serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,10 +3,10 @@
 # ./app/controllers/api/v1/merchants_controller
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
