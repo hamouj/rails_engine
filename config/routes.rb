8c1ad0f55cd2f629c71314927993c81bf3297d24
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-03-28 09:37:31 -0700

    Feat: complete /api/v1/items/:item_id/merchant endpoint and tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items
+      resources :items do
+        resources :merchant, only: [:index], controller: 'item/merchants'
+      end
+      
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchant/items'
       end
